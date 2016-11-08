035bef514027f7a82f55668751ccaf6cea01fa2e
Kelly James <kellyjamesclancy@gmail.com>
2016-11-08 02:02:45 +0000

    Sucessfully downloading images,

--- http_client_example.js
+++ http_client_example.js
@@ -1,11 +1,16 @@
 const request = require('request');
-const request = require('fs');
+const fs = require('fs');
 
-request.get('https://sytantris.github.io/http-examples/')
+request.get('https://sytantris.github.io/http-examples/future.jpg')
       .on('error', function(err) {
         throw err;
       })
       .on('response', function(response) {
-        console.log('Response Status Code: ', response.statusCode);
+        console.log('Response Status Message: ', response.statusMessage);
+        console.log('Response Content-Type: ', response.headers['content-type']);
+        console.log('Downloading image ... ');
+        console.log('Download Complete.');
       })
-      .pipe(fs.createWriteStream('./download.html'));
+      .pipe(fs.createWriteStream('./future.jpg'))
+      // .on('end', () => {
+      // })
